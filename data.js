657948d708f3bc25552a06809ef22e32bfca4409
cruzrafaelx <rafaelcruz@outlook.de>
2024-04-22 04:32:15 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,52 @@
+export const blogs = [ 
+      {     
+            data: "January 1, 2024",
+            name: "Blog 1",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            image: "images/blog-1.jpg",
+            uuid: 1
+      },
+
+      {     
+            data: "January 2, 2024",
+            name: "Blog 2",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            image: "images/blog-2.jpg",
+            uuid: 2
+      },
+
+      {     
+            data: "January 3, 2024",
+            name: "Blog 3",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            image: "images/blog-3.jpg",
+            uuid: 3
+      },
+
+      {     
+            data: "January 1, 2024",
+            name: "Blog 1",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            image: "images/blog-1.jpg",
+            uuid: 1
+      },
+
+      {     
+            data: "January 2, 2024",
+            name: "Blog 2",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            image: "images/blog-2.jpg",
+            uuid: 2
+      },
+
+      {     
+            data: "January 3, 2024",
+            name: "Blog 3",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            image: "images/blog-3.jpg",
+            uuid: 3
+      }
+
+]
+     
+     
