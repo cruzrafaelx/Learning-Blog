c883846e6d7d4cbb5ef741386e69bd5754de729c
cruzrafaelx <rafaelcruz@outlook.de>
2024-04-23 02:54:03 +0200

    Rename image files in data.js

--- data.js
+++ data.js
@@ -24,26 +24,26 @@ export const blogs = [
       },
 
       {     
-            date: "January 1, 2024",
+            date: "January 4, 2024",
             name: "Blog 4",
             description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
-            image: "images/blog-1.jpg",
+            image: "images/blog-4.jpg",
             uuid: 4
       },
 
       {     
-            date: "January 2, 2024",
+            date: "January 5, 2024",
             name: "Blog 5",
             description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
-            image: "images/blog-2.jpg",
+            image: "images/blog-5.jpg",
             uuid: 5
       },
 
       {     
-            date: "January 3, 2024",
+            date: "January 6, 2024",
             name: "Blog 6",
             description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
-            image: "images/blog-3.jpg",
+            image: "images/blog-6.jpg",
             uuid: 6
       }
 
